208357c55e2ebcdf5d106f59a1c04ea6969f7226
macray <macraykorir254@gmail.com>
2023-09-01 16:59:14 +0300

    Completed

---
+++ migrations/versions/9fd70c991463_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 9fd70c991463
+Revises: 
+Create Date: 2023-09-01 16:39:04.842454
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9fd70c991463'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
